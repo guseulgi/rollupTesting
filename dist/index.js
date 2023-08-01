07ec11564a1d9ac95c9bbf184ab38b00ae2bc160
seulgi <guseul9405@gmail.com>
2023-08-01 23:51:56 +0900

    Feat: @babel 패키지 설치 및 package.json 으로 @babel 설정

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.sum = void 0;
+var _testing = require("./testing");
+var sum = (0, _testing.add)(3, 4);
+exports.sum = sum;
