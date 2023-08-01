d8f366f11da7f0ab7138ebe19d634e916e94cd3b
seulgi <guseul9405@gmail.com>
2023-08-02 00:05:20 +0900

    Feat: @rollup/babel-plugin 으로 바벨 후 롤업해주는 과정을 플러그인 처리를 하여 한단계로 단축화

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.sum = void 0;
-var _testing = require("./testing");
-var sum = (0, _testing.add)(3, 4);
-exports.sum = sum;
--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,13 @@
+import babel from "@rollup/plugin-babel";
+
 const config = {
-  input: "dist/index.js",
+  input: "src/index.js",
   output: {
     file: "lib/index.js",
-    format: "cjs", //life, cjs, esm(보통 사용되는 export { } 형태)
+    format: "esm", //life, cjs, esm(보통 사용되는 export { } 형태)
     //name: "rollupTest", //life 로 지정해줄 때 필요
   },
+  plugins: [babel()],
 };
 
 export default config;
