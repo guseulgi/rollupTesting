85f306afdac3e34e9528bee2c3ce1777c698acfc
seulgi <guseul9405@gmail.com>
2023-08-01 23:58:57 +0900

    Feat: babel 로 정리해준 dist 폴더에서 lib 폴더의 index.js 파일 하나로 롤업

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 const config = {
-  intput: "src/index.js",
+  input: "dist/index.js",
   output: {
     file: "lib/index.js",
-    format: "esm", //life, cjs, esm(보통 사용되는 export { } 형태)
+    format: "cjs", //life, cjs, esm(보통 사용되는 export { } 형태)
     //name: "rollupTest", //life 로 지정해줄 때 필요
   },
 };
