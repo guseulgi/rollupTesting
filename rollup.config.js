82730a36d4998dad283cff9a27bbb57ebb7389bf
seulgi <guseul9405@gmail.com>
2023-08-03 00:12:21 +0900

    Feat: @rollup/plugin-node-resolve, @rollup/plugin-commonjs 플러그인을 사용하여 빌드 시 import 된 라이브러리를 파일 내에 번들링하게 설정

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import babel from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
 const config = {
   input: "src/index.js",
@@ -7,7 +9,7 @@ const config = {
     format: "cjs", //iife, cjs, esm(보통 사용되는 export { } 형태)
     // name: "rollupTest", //iife 로 지정해줄 때 필요
   },
-  plugins: [babel()],
+  plugins: [babel(), resolve(), commonjs()],
 };
 
 export default config;
