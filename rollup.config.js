2b7413b8438260e16d11d20d3b971f90ee4571a7
seulgi <guseul9405@gmail.com>
2023-08-03 00:14:25 +0900

    Feat: rollup-plugin-terser 를 사용하여 번들링된 내용을 압축하는 플러그인 설정

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from "@rollup/plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
 
 const config = {
   input: "src/index.js",
@@ -9,7 +10,7 @@ const config = {
     format: "cjs", //iife, cjs, esm(보통 사용되는 export { } 형태)
     // name: "rollupTest", //iife 로 지정해줄 때 필요
   },
-  plugins: [babel(), resolve(), commonjs()],
+  plugins: [babel(), resolve(), commonjs(), terser()],
 };
 
 export default config;
