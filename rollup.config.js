082c131e5483a89aa60f9decb6a598cf8218827b
seulgi <guseul9405@gmail.com>
2023-08-03 23:33:40 +0900

    Feat: script 를 추가하여 production 일 때만 terser 플러그인을 실행되게끔 분기 처리

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,12 @@ const config = {
     format: "cjs", //iife, cjs, esm(보통 사용되는 export { } 형태)
     // name: "rollupTest", //iife 로 지정해줄 때 필요
   },
-  plugins: [babel(), resolve(), commonjs(), terser()],
+  plugins: [
+    babel(),
+    resolve(),
+    commonjs(),
+    process.env.NODE_ENV === "production" && terser(), // yarn deploy 를 실행해줬을 때만 실행됨
+  ],
   external: ["lodash"], // 라이브러리를 빼줄 때  사용
 };
 
